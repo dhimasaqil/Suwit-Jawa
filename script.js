f9e247eb8d3437461de97064f5ed4530a23d9fb6
Dhimas Aqil <17523133@students.uii.ac.id>
2022-04-05 21:42:32 +0700

    git commit -m "suwit jawa"

---
+++ script.js
@@ -0,0 +1,86 @@
+function getPilihanKomputer() {
+  const comp = Math.random();
+
+  if (comp < 0.34) return "gajah";
+  if (comp >= 0.34 && comp < 0.67) return "orang";
+  return "semut";
+}
+
+function getHasil(comp, player) {
+  if (player === comp) return "Seri!";
+  if (player === "gajah") return comp === "orang" ? "Menang!" : "Kalah!";
+  if (player === "orang") return comp === "semut" ? "Menang!" : "Kalah!";
+  if (player === "semut") return comp === "gajah" ? "Menang!" : "Kalah!";
+}
+
+function putar() {
+  const imgComputer = document.querySelector(".img-komputer");
+  const gambar = ["gajah", "orang", "semut"];
+  let i = 0;
+  const waktuMulai = new Date().getTime();
+  setInterval(function () {
+    if (new Date().getTime() - waktuMulai > 1000) {
+      clearInterval;
+      return;
+    }
+    imgComputer.setAttribute("src", "/img/" + gambar[i++] + ".png");
+    if (i === gambar.length) i = 0;
+  }, 100);
+}
+
+const pilihan = document.querySelectorAll("li img");
+pilihan.forEach(function (pil) {
+  pil.addEventListener("click", function () {
+    const pilihanKomputer = getPilihanKomputer();
+    const pilihanPlayer = pil.className;
+    const hasil = getHasil(pilihanKomputer, pilihanPlayer);
+
+    putar();
+
+    setTimeout(function () {
+      const imgComputer = document.querySelector(".img-komputer");
+      imgComputer.setAttribute("src", "img/" + pilihanKomputer + ".png");
+
+      const info = document.querySelector(".info");
+      info.innerHTML = hasil;
+    }, 1000);
+  });
+});
+// const pGajah = document.querySelector(".gajah");
+// pGajah.addEventListener("click", function () {
+// const pilihanKomputer = getPilihanKomputer();
+// const pilihanPlayer = pGajah.className;
+// const hasil = getHasil(pilihanKomputer, pilihanPlayer);
+
+// const imgComputer = document.querySelector(".img-komputer");
+// imgComputer.setAttribute("src", "img/" + pilihanKomputer + ".png");
+
+// const info = document.querySelector(".info");
+// info.innerHTML = hasil;
+// });
+
+// const pOrang = document.querySelector(".orang");
+// pOrang.addEventListener("click", function () {
+//   const pilihanKomputer = getPilihanKomputer();
+//   const pilihanPlayer = pOrang.className;
+//   const hasil = getHasil(pilihanKomputer, pilihanPlayer);
+
+//   const imgComputer = document.querySelector(".img-komputer");
+//   imgComputer.setAttribute("src", "img/" + pilihanKomputer + ".png");
+
+//   const info = document.querySelector(".info");
+//   info.innerHTML = hasil;
+// });
+
+// const pSemut = document.querySelector(".semut");
+// pSemut.addEventListener("click", function () {
+//   const pilihanKomputer = getPilihanKomputer();
+//   const pilihanPlayer = pSemut.className;
+//   const hasil = getHasil(pilihanKomputer, pilihanPlayer);
+
+//   const imgComputer = document.querySelector(".img-komputer");
+//   imgComputer.setAttribute("src", "img/" + pilihanKomputer + ".png");
+
+//   const info = document.querySelector(".info");
+//   info.innerHTML = hasil;
+// });
